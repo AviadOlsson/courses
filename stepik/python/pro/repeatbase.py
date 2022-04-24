bb8c2926cfcbbe8fad3f38ea337330bec86d5fd8
AviadOlsson <79424758+AviadOlsson@users.noreply.github.com>
2022-04-24 20:18:35 +0300

    Update repeatbase.py

---
+++ stepik/python/pro/repeatbase.py
@@ -0,0 +1,47 @@
+#2.1-8
+"""
+def iosif_flav(num_a, num_b):
+    count = 0
+    for i in range(1, num_a + 1):
+        count = (count + num_b) % i
+    return count + 1
+
+print(iosif_flav(int(input()), int(input())))
+"""
+
+#2.2-1
+"""
+def quarters_of_dots(quantity):
+
+    quarter_i, quarter_ii, quarter_iii, quarter_iv = 0, 0, 0, 0
+
+    for i in range(quantity):
+        coords = input().split(" ")
+        if int(coords[0]) < 0 and int(coords[1]) > 0:
+            quarter_ii += 1
+        elif int(coords[0]) > 0 and int(coords[1]) > 0:
+            quarter_i += 1
+        elif int(coords[0]) < 0 and int(coords[1]) < 0:
+            quarter_iii += 1
+        elif int(coords[0]) > 0 and int(coords[1]) < 0:
+            quarter_iv += 1
+
+    return print(f'Первая четверть: {quarter_i}\nВторая четверть: {quarter_ii}\nТретья четверть: {quarter_iii}\nЧетвертая четверть: {quarter_iv}\n')
+
+quarters_of_dots(int(input()))
+"""
+
+#2.2-2
+"""
+list_of_numbers = input().split(" ")
+
+counter = 0
+for i in range(len(list_of_numbers)):
+    if i + 1 >= len(list_of_numbers):
+        break
+    else:
+        if int(list_of_numbers[i + 1]) > int(list_of_numbers[i]):
+            counter += 1
+    
+print(counter)
+"""
