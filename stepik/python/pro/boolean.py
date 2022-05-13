855027a92ac12e5d5ce0fe9d40c54d655ab45029
AviadOlsson <79424758+AviadOlsson@users.noreply.github.com>
2022-05-13 17:44:12 +0300

    Update boolean.py

--- stepik/python/pro/boolean.py
+++ stepik/python/pro/boolean.py
@@ -1,1 +1,28 @@
 #Задачи из темы булево (раздел 3 курса)
+
+#3.1-1
+""" решение без шаблона
+
+def func(num1, num2):
+    if num1 % num2 == 0:
+        return print('делится')
+    else:
+        return print('не делится')
+"""
+""" решение с шаблоном stepik
+
+# объявление функции
+def func(num1, num2):
+    return num1 % num2 == 0
+
+# считываем данные
+num1, num2 = int(input()), int(input())
+
+# вызываем функцию
+if func(num1, num2):
+    print('делится')
+else:
+    print('не делится')
+"""
+
+#
