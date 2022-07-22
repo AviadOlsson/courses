e622b18dbb511352acee4d2dea187f79b4bde036
AviadOlsson <79424758+AviadOlsson@users.noreply.github.com>
2022-07-22 12:12:02 +0300

    Update matrix_chapter1.py

--- stepik/python/pro/matrix_chapter1.py
+++ stepik/python/pro/matrix_chapter1.py
@@ -1,10 +1,25 @@
 #4.4-1
+"""
 a, b = int(input()), int(input())
 
-ResultMatrix = [[]]
+ResultMatrix = [[""] * b for _ in range(a)]
 
-for i in range(a - 1):
-    for j in range(b - 1):
-        ResultMatrix[i][j].append(input())
+for i in range(a):
+    for j in range(b):
+        ResultMatrix[i][j] = input()
+    print(*ResultMatrix[i])
+"""
 
-print(ResultMatrix)
+#4.4-2
+a, b = int(input()), int(input())
+
+ResultMatrix = [[""] * b for _ in range(a)]
+
+for i in range(a):
+    for j in range(b):
+        ResultMatrix[i][j] = input()
+    print(*ResultMatrix[i])
+
+for i in range(b):
+    for j in range(a):
+        print(*ResultMatrix[j][i])
