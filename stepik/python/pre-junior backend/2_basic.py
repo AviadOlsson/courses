6eb1d752eaff870c2f9f3dba14964c84f6649c7d
AviadOlsson <79424758+AviadOlsson@users.noreply.github.com>
2022-11-21 03:59:04 +0300

    Update 2_basic.py

--- stepik/python/pre-junior backend/2_basic.py
+++ stepik/python/pre-junior backend/2_basic.py
@@ -35,3 +35,68 @@ print(oct(num))  # 0o45
 print(hex(num))  # 0x25
 """
 
+#2.2
+"""
+a = int(input())
+
+def aOctInt(num):
+    aInt = num // 8
+    aRem = num % 8
+    return print(aInt, aRem, sep='')
+
+aOctInt(a)
+"""
+
+#2.3
+"""
+time_in_minutes = int(input())
+
+def time_in_hour(time):
+    return print(time // 60, time % 60)
+
+time_in_hour(time_in_minutes)
+"""
+
+#2.4
+"""
+time_mins, start_hours, start_mins = int(input()), int(input()), int(input())
+
+def time_out(time_on_mins, start_in_hours, start_in_mins):
+    
+    hours = 0
+    minutes = 0
+
+    if start_in_hours + time_on_mins // 60 > 24:
+        hours = (start_in_hours + time_on_mins // 60) - 24
+    else:
+        hours = start_in_hours + time_on_mins // 60
+    
+    if start_in_mins + time_on_mins % 60 > 60:
+        minutes = (start_in_mins + time_on_mins % 60) - 60
+        hours += 1
+    else:
+        minutes = start_in_mins + time_on_mins % 60
+
+    return print(hours, minutes)
+
+time_out(time_mins, start_hours, start_mins)
+"""
+
+#2.5
+"""
+number = int(input())
+
+def third_number(input_n):
+    return print(str(input_n)[1])
+
+third_number(number)
+"""
+
+#2.6
+
+float_number = input()
+
+def two_number_after_dot(input_n):
+    return print(int(input_n) % 100)
+
+two_number_after_dot(float_number)
